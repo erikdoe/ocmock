6ee50b5276624545108fcf204ad277af5bc58b48
Erik Doernenburg <erik@doernenburg.com>
2008-03-18 07:06:17 +0000

    Using new functions to deal with protocols
    git-svn-id: http://svn.mulle-kybernetik.com/OCMock/trunk@22 12620d81-68e2-0310-ae40-e3f550779089

--- Source/OCProtocolMockObject.m
+++ Source/OCProtocolMockObject.m
@@ -3,7 +3,7 @@
 //  Copyright (c) 2005-2008 by Mulle Kybernetik. See License file for details.
 //---------------------------------------------------------------------------------------
 
-#import <objc/Protocol.h>
+#import <objc/runtime.h>
 #import "NSMethodSignature+Private.h"
 #import "OCProtocolMockObject.h"
 
@@ -38,15 +38,26 @@
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
 {
-	struct objc_method_description *desc = [mockedProtocol descriptionForInstanceMethod:aSelector];
-	if (desc == NULL)
-	   return nil;
-	return [NSMethodSignature signatureWithObjCTypes:desc->types];
+	struct objc_method_description methodDescription = protocol_getMethodDescription(mockedProtocol, aSelector, YES, YES);
+    if(methodDescription.name == NULL) 
+	{
+        methodDescription = protocol_getMethodDescription(mockedProtocol, aSelector, NO, YES);
+    }
+    if(methodDescription.name == NULL) 
+	{
+        return nil;
+    }
+	return [NSMethodSignature signatureWithObjCTypes:methodDescription.types];
 }
 
 - (BOOL)conformsToProtocol:(Protocol *)aProtocol
 {
-	return [mockedProtocol conformsTo:aProtocol];
+    return protocol_conformsToProtocol(mockedProtocol, aProtocol);
+}
+
+- (BOOL)respondsToSelector:(SEL)selector
+{
+    return ([self methodSignatureForSelector:selector] != nil);
 }
 
 
