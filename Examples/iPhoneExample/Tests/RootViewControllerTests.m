c5b37cc3954394c8ac55754259387f5e0b39b71f
Erik Doernenburg <erik@doernenburg.com>
2010-07-20 13:14:31 +0000

    Initial stab at an iPhone example. Has issues.
    git-svn-id: http://svn.mulle-kybernetik.com/OCMock/trunk@59 12620d81-68e2-0310-ae40-e3f550779089

---
+++ Examples/iPhoneExample/Tests/RootViewControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  RootViewControllerTests.m
+//  iPhoneExample
+//
+//  Created by Erik Doernenburg on 20/07/10.
+//  Copyright 2010 Mulle Kybernetik. All rights reserved.
+//
+
+#import <OCMock/OCMock.h>
+#import "RootViewControllerTests.h"
+#import "RootViewController.h"
+
+
+@implementation RootViewControllerTests
+
+- (void)testControllerReturnsCorrectNumberOfRows
+{
+	RootViewController *controller = [[[RootViewController alloc] initWithStyle:UITableViewStylePlain] autorelease];
+	
+	STAssertEquals(1, [controller tableView:nil numberOfRowsInSection:0], @"Should have returned correct number of rows.");
+}
+
+- (void)testControllerSetsUpCellCorrectly
+{
+	RootViewController *controller = [[[RootViewController alloc] initWithStyle:UITableViewStylePlain] autorelease];
+	id mockTableView = [OCMockObject mockForClass:[UITableView class]];
+	[[[mockTableView expect] andReturn:nil] dequeueReusableCellWithIdentifier:@"HelloWorldCell"];
+	
+	UITableViewCell *cell = [controller tableView:mockTableView cellForRowAtIndexPath:nil];
+//	UITableViewCell *cell = [controller tableView:nil cellForRowAtIndexPath:nil];
+	
+	STAssertNotNil(cell, @"Should have returned a cell");
+//	STAssertNotNil(cell.textLabel, @"Cell should have a text label");
+//	STAssertEquals(@"Hello World!", cell.textLabel.text, @"Should have set up cell correctly");
+	[mockTableView verify];
+}
+
+
+
+@end
