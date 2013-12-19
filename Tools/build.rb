059f55e1604a98eae740a2d2e1e8fcec3ef29858
Erik Doernenburg <erik@doernenburg.com>
2013-12-19 16:56:27 +0100

    Preparation for 2.2.2 release.

--- Tools/build.rb
+++ Tools/build.rb
@@ -12,7 +12,7 @@ class Builder
       downloadSource
       copySource
       buildModules
-      createPackage "ocmock-2.2.1.dmg", "OCMock 2.2.1" 
+      createPackage "ocmock-2.2.2.dmg", "OCMock 2.2.2" 
       openPackageDir
     end
     
