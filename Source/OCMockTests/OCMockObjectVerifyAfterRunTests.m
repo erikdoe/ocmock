18a3c6413a20773d6c34e07ffab8b2e3fd1f708f
Erik Doernenburg <erik@doernenburg.com>
2014-05-04 14:41:57 +0200

    Tests showing how much partial mocks work so far.

--- Source/OCMockTests/OCMockObjectVerifyAfterRunTests.m
+++ Source/OCMockTests/OCMockObjectVerifyAfterRunTests.m
@@ -5,10 +5,33 @@
 #import <XCTest/XCTest.h>
 #import "OCMockObject.h"
 
+@interface TestClassForVerifyAfterRun : NSObject
+
+- (NSString *)method1;
+- (NSString *)method2;
+
+@end
+
+@implementation TestClassForVerifyAfterRun
+
+- (NSString *)method1
+{
+	id retVal = [self method2];
+	return retVal;
+}
+
+- (NSString *)method2
+{
+	return @"Foo";
+}
+
+@end
+
 
 @interface OCMockObjectVerifyAfterRunTests : XCTestCase
 
 @end
+
 
 @implementation OCMockObjectVerifyAfterRunTests
 
@@ -29,5 +52,25 @@
 
     XCTAssertThrows([[mock verify] uppercaseString], @"Should have thrown an exception for a method that was not called.");
 }
+
+- (void)testDoesNotThrowWhenMethodWasInvokedOnPartialMock
+{
+    TestClassForVerifyAfterRun *testObject = [[[TestClassForVerifyAfterRun alloc] init] autorelease];
+    id mock = [OCMockObject partialMockForObject:testObject];
+
+    [mock method2];
+
+    XCTAssertNoThrow([[mock verify] method2], @"Should not have thrown an exception for method that was called.");
+}
+
+//- (void)testDoesNotThrowWhenMethodWasInvokedDirectlyOnRealObject
+//{
+//    TestClassForVerifyAfterRun *testObject = [[[TestClassForVerifyAfterRun alloc] init] autorelease];
+//    id mock = [OCMockObject partialMockForObject:testObject];
+//
+//    [mock method1];
+//
+//    XCTAssertNoThrow([[mock verify] method2], @"Should not have thrown an exception for method that was called.");
+//}
 
 @end
