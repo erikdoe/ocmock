81c75cc465f8f4df15bdca3534777c9b242cd5eb
Erik Doernenburg <erik@doernenburg.com>
2014-05-22 21:06:01 +0200

    Excluded one assertion on platform where it fails.

--- Source/OCMockTests/OCMArgTests.m
+++ Source/OCMockTests/OCMArgTests.m
@@ -24,15 +24,15 @@
 {
     NSRange range = NSMakeRange(5, 5);
     XCTAssertEqualObjects(OCMOCK_VALUE(range), [NSValue valueWithRange:range]);
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
-    /* Should work with constant values and some expressions */
-    XCTAssertEqualObjects(OCMOCK_VALUE(YES), @YES);
+#if !(TARGET_OS_IPHONE && TARGET_RT_64_BIT)
+    /* This should work everywhere but I can't get it to work on iOS 64-bit */
+    XCTAssertEqualObjects(OCMOCK_VALUE((BOOL){YES}), @YES);
+#endif
     XCTAssertEqualObjects(OCMOCK_VALUE(42), @42);
 #if !TARGET_OS_IPHONE
     XCTAssertEqualObjects(OCMOCK_VALUE(NSZeroRect), [NSValue valueWithRect:NSZeroRect]);
 #endif
     XCTAssertEqualObjects(OCMOCK_VALUE([@"0123456789" rangeOfString:@"56789"]), [NSValue valueWithRange:range]);
-#endif
 }
 
 @end
