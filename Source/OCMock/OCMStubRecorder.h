c32abcaba428921d8ac12ac98b272ecf8241f9bb
Jonathan Crooke <joncrooke@gmail.com>
2015-09-10 15:25:35 +0200

    Runtime check of value type rather than static

--- Source/OCMock/OCMStubRecorder.h
+++ Source/OCMock/OCMStubRecorder.h
@@ -35,7 +35,7 @@
 #define andReturn(aValue) _andReturn(({                                             \
   __typeof__(aValue) _val = (aValue);                                               \
   NSValue *_nsval = [NSValue value:&_val withObjCType:@encode(__typeof__(_val))];   \
-  if (__builtin_types_compatible_p(__typeof__(_val), id)) {                         \
+  if (OCMIsObjectType(@encode(__typeof(_val)))) {                                   \
       objc_setAssociatedObject(_nsval, "OCMAssociatedBoxedValue", *(__unsafe_unretained id *) (void *) &_val, OBJC_ASSOCIATION_RETAIN); \
   }                                                                                 \
   _nsval;                                                                           \
