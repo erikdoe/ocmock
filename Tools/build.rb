63ccee8105bb286f50aff6efe44e8692024697c0
Erik Doernenburg <erik@doernenburg.com>
2016-01-20 23:16:50 +0100

    Preparation for 3.2.2 release.

--- Tools/build.rb
+++ Tools/build.rb
@@ -13,7 +13,7 @@ class Builder
       copySource
       buildModules
       signFrameworks "erik@doernenburg.com"
-      createPackage "ocmock-3.2.1.dmg", "OCMock 3.2.1" 
+      createPackage "ocmock-3.2.2.dmg", "OCMock 3.2.2" 
       sanityCheck
       openPackageDir
     end
