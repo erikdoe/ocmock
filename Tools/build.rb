d7043cccbad82dd6a98d30b9175e75a0cc29953b
Erik Doernenburg <erik@doernenburg.com>
2020-02-16 14:09:53 +0100

    Preparation for 3.6 release.

--- Tools/build.rb
+++ Tools/build.rb
@@ -13,7 +13,7 @@ class Builder
       copySource
       buildModules
       signFrameworks "erik@doernenburg.com"
-      createPackage "ocmock-3.5.dmg", "OCMock 3.5"
+      createPackage "ocmock-3.6.dmg", "OCMock 3.6"
       sanityCheck
       openPackageDir
     end
